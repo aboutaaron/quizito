4b061210df057b018d11ee7e7e53352b613262c2
Aaron Williams <aaron.colby.williams@gmail.com>
2014-08-04 10:38:28 -0700

    newsquiz file

---
+++ src/newsquiz.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function () {
+    var NewsQuiz = function (selector, questions) {
+        this.selector = document.querySelector(selector);
+        this.questions = questions;
+    }
+    // events
+}).call(this);
