aa18ddf26d67dedbbbee8b12cbcd2eac01335e7f
Aaron Williams <aaron.colby.williams@gmail.com>
2014-08-04 15:26:53 -0700

    looks like this library works. getting the API finalized. Keeping it basic.

--- src/newsquiz.js
+++ src/newsquiz.js
@@ -2,18 +2,79 @@ function NewsQuiz (container, questions) {
     'use strict';
 
     this.container = document.querySelector(container);
+    // Store questions to object
     this.questions = questions;
-    this.setupHtml();
+    // Kickoff
+    this.init();
 }
 
 NewsQuiz.prototype = {
-    setupHtml: function () {
-        var template = '<div class="quiz-body"><div class="quiz-question">Have you done x?</div><div class="quiz-answer"><input type="radio" name="quiz-radio" id="quiz-yes" value="yes"> Yes <input type="radio" name="quiz-radio" id="quiz-no" value="no"> No </div> <div class="quiz-controls"> <a href="#" class="quiz-prev">prev</a> <a href="#" class="quiz-next">next</a> </div> </div>';
+    init: function () {
+        var that = this;
 
-        this.container.innerHTML = template;
+        that.state = 'start';
+        that.count = 0;
+        // Select the container for the quiz
+        // answers submitted by user
+        that.answers = [];
+
+        // Load Template
+        that.buildTemplate('<div class="quiz-body"><div class="quiz-question">Question></div><div class="quiz-answer"><input type="radio" name="quiz-radio" id="quiz-yes" value="yes"> Yes <input type="radio" name="quiz-radio" id="quiz-no" value="no"> No </div> <div class="quiz-controls"> <a href="#" class="quiz-submit">submit</a> </div> </div>');
+
+        // Cycle through questions
+        that.cycle();
+
+        // Attach default event listeners
+        that.on('change', function () {
+            // Push values to array
+            that.answers.push( {count: that.count, question: that.questions[that.count - 1].question, answer: this.value} );
+        });
+
+        // Cycle to the next question when a user submits an answer
+        that.on('submit', function (event) {
+            event.preventDefault();
+            that.cycle();
+        });
+
+        // Watch events that listen to property changes
+        that.watch('count', function (id, oldval, newval) { return newval; });
+        that.watch('state', function (id, oldval, newval) { return newval; });
+    },
+    reset: function () {
+        var that = this,
+            btn = that.container.querySelector('.quiz-retry');
+
+        btn.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            that.init();
+        });
+    },
+    buildTemplate: function (string) {
+        this.container.innerHTML = string;
+    },
+    loadQuestion: function (obj) {
+        var questionContainer = this.container.querySelector('.quiz-question');
+        questionContainer.innerHTML = obj.question;
+    },
+    cycle: function () {
+        var that = this;
+
+        if (that.count < that.questions.length) {
+            that.loadQuestion(that.questions[that.count]);
+            that.count++;
+            that.state = 'active';
+
+        } else {
+            // Load results page
+            that.state = 'finished';
+            that.buildTemplate('<div class="quiz-body" id="results"><div class="quiz-question">Results</div><div class="quiz-results-body"></div><a href="#" class="quiz-retry">Try again</a></div>');
+
+            that.reset();
+        }
     },
     on: function (eventName, eventHandler) {
-
+        // Event for when radio buttons change
         if (eventName === 'change') {
             var radios = this.container.querySelectorAll('input');
 
@@ -24,10 +85,67 @@ NewsQuiz.prototype = {
                     radio.addEventListener('change', eventHandler);
                 }
             );
-        } else {
-
-            this.container.addEventListener(eventName, eventHandler);
         }
 
+        // Event for when button is change
+        if (eventName === 'submit') {
+            var submit = this.container.querySelector('.quiz-submit');
+            submit.addEventListener('click', eventHandler);
+        }
     }
 };
+
+/*
+ * object.watch polyfill
+ *
+ * 2012-04-03
+ *
+ * By Eli Grey, http://eligrey.com
+ * Public Domain.
+ * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+ */
+
+// object.watch
+if (!Object.prototype.watch) {
+    Object.defineProperty(Object.prototype, "watch", {
+          enumerable: false
+        , configurable: true
+        , writable: false
+        , value: function (prop, handler) {
+            var
+              oldval = this[prop]
+            , newval = oldval
+            , getter = function () {
+                return newval;
+            }
+            , setter = function (val) {
+                oldval = newval;
+                return newval = handler.call(this, prop, oldval, val);
+            }
+            ;
+
+            if (delete this[prop]) { // can't watch constants
+                Object.defineProperty(this, prop, {
+                      get: getter
+                    , set: setter
+                    , enumerable: true
+                    , configurable: true
+                });
+            }
+        }
+    });
+}
+
+// object.unwatch
+if (!Object.prototype.unwatch) {
+    Object.defineProperty(Object.prototype, "unwatch", {
+          enumerable: false
+        , configurable: true
+        , writable: false
+        , value: function (prop) {
+            var val = this[prop];
+            delete this[prop]; // remove accessors
+            this[prop] = val;
+        }
+    });
+}
