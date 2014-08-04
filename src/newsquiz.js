b284c3921eef4f6cbd471d37a3f9661c622e8127
Aaron Williams <aaron.colby.williams@gmail.com>
2014-08-04 12:01:25 -0700

    example page with bootstrap

--- src/newsquiz.js
+++ src/newsquiz.js
@@ -1,9 +1,33 @@
-(function () {
+function NewsQuiz (container, questions) {
     'use strict';
 
-    var NewsQuiz = function (selector, questions) {
-        this.selector = document.querySelector(selector);
-        this.questions = questions;
-    };
+    this.container = document.querySelector(container);
+    this.questions = questions;
+    this.setupHtml();
+}
 
-}).call(this);
+NewsQuiz.prototype = {
+    setupHtml: function () {
+        var template = '<div class="quiz-body"><div class="quiz-question">Have you done x?</div><div class="quiz-answer"><input type="radio" name="quiz-radio" id="quiz-yes" value="yes"> Yes <input type="radio" name="quiz-radio" id="quiz-no" value="no"> No </div> <div class="quiz-controls"> <a href="#" class="quiz-prev">prev</a> <a href="#" class="quiz-next">next</a> </div> </div>';
+
+        this.container.innerHTML = template;
+    },
+    on: function (eventName, eventHandler) {
+
+        if (eventName === 'change') {
+            var radios = this.container.querySelectorAll('input');
+
+            // Iterate over radios and attach event listeners
+            [].forEach.call(
+                radios,
+                function (radio) {
+                    radio.addEventListener('change', eventHandler);
+                }
+            );
+        } else {
+
+            this.container.addEventListener(eventName, eventHandler);
+        }
+
+    }
+};
